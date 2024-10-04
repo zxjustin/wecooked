9b74d0137d4fc0886e2be33b7b4e9c6405dab958
zxjustin <zxjustin@hotmail.com>
2024-10-04 17:22:22 +0800

    Added Recipes Classes
    Static Hive database of recipe, and ui interaction of the scenarios

--- wecooked/lib/node.g.dart
+++ wecooked/lib/node.g.dart
@@ -19,7 +19,7 @@ class RecipeAdapter extends TypeAdapter<Recipe> {
     return Recipe(
       name: fields[0] as String,
       ingredients: (fields[1] as List).cast<String>(),
-      steps: (fields[2] as List).cast<String>(),
+      stepScenarios: (fields[2] as List).cast<StepScenario>(),
     );
   }
 
@@ -32,7 +32,7 @@ class RecipeAdapter extends TypeAdapter<Recipe> {
       ..writeByte(1)
       ..write(obj.ingredients)
       ..writeByte(2)
-      ..write(obj.steps);
+      ..write(obj.stepScenarios);
   }
 
   @override
@@ -42,6 +42,46 @@ class RecipeAdapter extends TypeAdapter<Recipe> {
   bool operator ==(Object other) =>
       identical(this, other) ||
       other is RecipeAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class StepScenarioAdapter extends TypeAdapter<StepScenario> {
+  @override
+  final int typeId = 1;
+
+  @override
+  StepScenario read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return StepScenario(
+      question: fields[0] as String,
+      options: (fields[1] as List).cast<String>(),
+      correctOptionIndex: fields[2] as int,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, StepScenario obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.question)
+      ..writeByte(1)
+      ..write(obj.options)
+      ..writeByte(2)
+      ..write(obj.correctOptionIndex);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is StepScenarioAdapter &&
           runtimeType == other.runtimeType &&
           typeId == other.typeId;
 }
